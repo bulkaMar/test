466c0f02abdfd2702b51f291dcd4bad54f103c5b
Маріна Булах <marinabulah@MacBook-Pro-Marina.local>
2025-02-11 18:33:19 +0200

    change some files

--- js/features.js
+++
@@ -1,8 +0,0 @@
-// const featureItems = document.querySelectorAll(".features__item");
-
-// featureItems.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     featureItems.forEach((el) => el.classList.remove("features__item--active"));
-//     item.classList.add("features__item--active");
-//   });
-// });
